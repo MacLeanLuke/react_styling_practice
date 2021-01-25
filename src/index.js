ffa654e2ea872b27be51c3fa5549d5f1e6deaff3
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-01-25 12:16:08 -0500

    Using css stylesheet and inline style to format react elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+
 //Create a React app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Deterine current local time
+const currentTime = new Date().getHours();
+
+//Morning = red, Afternoon = green, Night = blue.
+const customStyle = {
+  color: ""
+};
+
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
 //or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
+let greeting;
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
